ca9e92c3ab06f7e9e8f837e04301248d3a6ebff5
heiMartin <maness@stud.ntnu.no>
2026-02-23 09:44:57 +0100

    Added pointers to stat_sequence

--- skeleton_project/source/driver/start_sequence.c
+++ skeleton_project/source/driver/start_sequence.c
@@ -3,13 +3,32 @@
 #include "elevio.h"
 #include "con_load.h"
 
+//initialisering
 elevio_init()
-ButtonType Order_buttons[] = {BUTTON_HALL_DOWN, BUTTON_CAB, BUTTON_HALL_DOWN, BUTTON_HALL_UP,
-     BUTTON_CAB, BUTTON_HALL_DOWN, BUTTON_HALL_UP, BUTTON_CAB, BUTTON_HALL_UP, BUTTON_CAB};
+int g_stop_buttoninit= 0;
+int g_floor_sensorinit = elevio_floorSensor();
+int g_obstructioninit = 0;
+int g_order_buttons[12] = {0};
 
-int current_floor = elevio_floorSensor()
-elevio_motorDirection(-1)
-while current_floor != 1
+int *p_g_stop_buttons = &g_stop_buttoninit
+int *p_g_floor_sensorinit = &g_floor_sensorinit
+int *p_g_obstructioninit = &g_obstructioninit
+int *g_order_buttons = &g_order_buttons
+
+
+
+elevio_motorDirection(-1);
+while current_floor != 1{
     current_floor = elevio_floorSensor()
+}
+elevio_motorDirection(0);
 
-elevio_motorDirection(0)
+
+
+//Main program
+while (1){
+    info_gathering()
+    if stop_button(){
+        while
+    }
+}
